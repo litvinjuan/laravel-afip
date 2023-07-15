33efc8c02274fd364f583c73a0dd6c1713ef15c5
Juan Litvin <litvinjuan@gmail.com>
2023-07-14 21:10:50 -0300

    Cleanup

--- src/LaravelAfipServiceProvider.php
+++ src/LaravelAfipServiceProvider.php
@@ -2,22 +2,21 @@
 
 namespace litvinjuan\LaravelAfip;
 
+use Illuminate\Support\Facades\Storage;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use litvinjuan\LaravelAfip\Commands\InstallLaravelAfipCommand;
 
 class LaravelAfipServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-afip')
             ->hasConfigFile()
-            ->hasCommand(InstallLaravelAfipCommand::class);
+            ->hasInstallCommand(function () {
+                Storage::disk(config('afip.certificates-disk'))->makeDirectory(config('afip.certificates-directory', ''));
+                Storage::disk(config('afip.certificates-disk'))->put(config('afip.certificates-directory'). '/cert', '');
+                Storage::disk(config('afip.certificates-disk'))->put(config('afip.certificates-directory'). '/key', '');
+            });
     }
 }
